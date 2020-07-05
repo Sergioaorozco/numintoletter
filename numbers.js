5ce00a8827598e79ab534e55976f7a9b8191569a
Sergio <sergioa.orozcoo@gmail.com>
2020-07-05 11:35:13 -0500

    Actualización código Javascript y botón copytoclipboard

--- numbers.js
+++ numbers.js
@@ -126,7 +126,7 @@ function Millones(num){
   cientos = Math.floor(num / divisor)
   resto = num - (cientos * divisor)
  
-  strMillones = Seccion(num, divisor, "UN MILLÓN", "MILLONES");
+  strMillones = Seccion(num, divisor, "UN MILLON", "MILLONES");
   strMiles = Miles(resto);
  
   if(strMillones == "")
@@ -145,11 +145,11 @@ function NumeroALetras(num,centavos){
     letrasCentavos: "",
   };
   if(centavos == undefined || centavos==false) {
-    data.letrasMonedaPlural="PESOS";
-    data.letrasMonedaSingular="PESO";
+    data.letrasMonedaPlural="EUROS";
+    data.letrasMonedaSingular="EURO";
   }else{
-    data.letrasMonedaPlural="CENTAVOS";
-    data.letrasMonedaSingular="CENTAVO";
+    data.letrasMonedaPlural="CENTIMOS";
+    data.letrasMonedaSingular="CENTIMO";
   }
  
   if (data.centavos > 0)
@@ -162,30 +162,3 @@ function NumeroALetras(num,centavos){
   else
     return Millones(data.enteros) + " " + data.letrasMonedaPlural + " " + data.letrasCentavos;
 }//NumeroALetras()
-
-           //Asigno el evento "click" del botón para provoar el copiado
-           document.getElementById('btnCopiar').addEventListener('click', copiarAlPortapapeles);
-
-           //Función que lanza el copiado del código
-           function myFunction(ev){
-               var codigoACopiar = document.getElementById('texto');    //Elemento a copiar
-               //Debe estar seleccionado en la página para que surta efecto, así que...
-               var seleccion = document.createRange(); //Creo una nueva selección vacía
-               seleccion.selectNodeContents(codigoACopiar);    //incluyo el nodo en la selección
-               //Antes de añadir el intervalo de selección a la selección actual, elimino otros que pudieran existir (sino no funciona en Edge)
-               window.getSelection().removeAllRanges();
-               window.getSelection().addRange(seleccion);  //Y la añado a lo seleccionado actualmente
-               try {
-                   var res = document.execCommand('copy'); //Intento el copiado
-                   if (res)
-                       exito();
-                   else
-                       fracaso();
-           
-                   mostrarAlerta();
-               }
-               catch(ex) {
-                   excepcion();
-               }
-               window.getSelection().removeRange(seleccion);
-           }
